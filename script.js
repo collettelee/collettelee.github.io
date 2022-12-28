30620fbd7e1562d63b458acb8f2745ca5b91a2b4
collettelee <98932808+collettelee@users.noreply.github.com>
2022-12-28 14:38:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
-
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
+const btn = document.querySelector("button"); 
 if (btn) {
   btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
     btn.classList.toggle("dipped");
   };
 }
 
 let slideIndex = [1,1];
-/* Class the members of each slideshow group with different CSS classes */
 let slideId = ["mySlides1", "mySlides2"]
 showSlides(1, 0);
 showSlides(1, 1);
@@ -33,15 +17,10 @@ function plusSlides(n, no) {
 function showSlides(n, no) {
   let i;
   let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}
+  if (n > x.length) {slideIndex[no] = 1}    
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+     x[i].style.display = "none";  
   }
-  x[slideIndex[no]-1].style.display = "block";
+  x[slideIndex[no]-1].style.display = "block";  
 }
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
