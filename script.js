991300e71f2f47a4c35e40abe08025bbda71c1b1
collettelee <98932808+collettelee@users.noreply.github.com>
2022-12-29 17:10:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector("button"); 
 
-let slideIndex = [1,1,1,1,1,1,1,1,1,1];
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10"]
+let slideIndex = [1,1,1,1,1,1,1,1,1,1,1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
@@ -12,6 +12,7 @@ showSlides(1, 6);
 showSlides(1, 7);
 showSlides(1, 8);
 showSlides(1, 9);
+showSlides(1,10);
 
 if (btn) {
   btn.onclick = function() {
