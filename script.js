c67434bf2cf9c408b2acd0fb044b343b411f99ad
collettelee <98932808+collettelee@users.noreply.github.com>
2023-06-25 22:41:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -43,8 +43,8 @@ document.addEventListener('DOMContentLoaded',function(event){
 
 const btn = document.querySelector("button"); 
 
-let slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12","mySlides13","mySlides14","mySlides15","mySlides16","mySlides17"]
+let slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12","mySlides13","mySlides14","mySlides15","mySlides16","mySlides17","mySlides18"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
@@ -62,6 +62,7 @@ showSlides(1,13);
 showSlides(1,14);
 showSlides(1,15);
 showSlides(1,16);
+showSlides(1,17);
 
 if (btn) {
   btn.onclick = function() {
