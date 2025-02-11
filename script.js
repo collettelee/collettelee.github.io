2109b26a97a197e6b34d1acb4f1e1fdebe13b8e3
collettelee <98932808+collettelee@users.noreply.github.com>
2025-02-10 21:24:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 1000); // Change image every 2 seconds
+  setTimeout(showSlides, 1000); // Change image every second
 }
