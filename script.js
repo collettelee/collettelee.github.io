cfef35f4570e8e4ddb4694bb1e7af30ef061c79c
collettelee <98932808+collettelee@users.noreply.github.com>
2025-02-10 21:01:53 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,87 +1,19 @@
-// document.addEventListener('DOMContentLoaded',function(event){
-//   // array with texts to type in typewriter
-//   var dataText = [ "Collette Lee", "Design", "Interactivity", "Creative Direction", "Collette Lee"];
-  
-//   // type one text in the typwriter
-//   // keeps calling itself until the text is finished
-//   function typeWriter(text, i, fnCallback) {
-//     // chekc if text isn't finished yet
-//     if (i < (text.length)) {
-//       // add next character to h1
-//      document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+let slideIndex = 0;
+showSlides();
 
-//       // wait for a while and call this function again for next character
-//       setTimeout(function() {
-//         typeWriter(text, i + 1, fnCallback)
-//       }, 100);
-//     }
-//     // text finished, call callback if there is a callback function
-//     else if (typeof fnCallback == 'function') {
-//       // call callback after timeout
-//       setTimeout(fnCallback, 700);
-//     }
-//   }
-//   // start a typewriter animation for a text in the dataText array
-//    function StartTextAnimation(i) {
-//      if (typeof dataText[i] == 'undefined'){
-//         setTimeout(function() {
-//           StartTextAnimation(0);
-//         }, 20000);
-//      }
-//      // check if dataText[i] exists
-//     if (i < dataText[i].length) {
-//       // text exists! start typewriter animation
-//      typeWriter(dataText[i], 0, function(){
-//        // after callback (and whole text has been animated), start next text
-//        StartTextAnimation(i + 1);
-//      });
-//     }
-//   }
-//   // start the text animation
-//   StartTextAnimation(0);
-// });
-
-const btn = document.querySelector("button"); 
-
-let slideIndex = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12","mySlides13","mySlides14","mySlides15","mySlides16","mySlides17","mySlides18"]
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-showSlides(1, 3);
-showSlides(1, 4);
-showSlides(1, 5);
-showSlides(1, 6);
-showSlides(1, 7);
-showSlides(1, 8);
-showSlides(1, 9);
-showSlides(1,10);
-showSlides(1,11);
-showSlides(1,12);
-showSlides(1,13);
-showSlides(1,14);
-showSlides(1,15);
-showSlides(1,16);
-showSlides(1,17);
-
-if (btn) {
-  btn.onclick = function() {
-    btn.classList.toggle("dipped");
-  };
-}
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
-
-function showSlides(n, no) {
+function showSlides() {
   let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-  x[slideIndex[no]-1].style.display = "block";
-  setTimeout(showSlides, 2000);
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
